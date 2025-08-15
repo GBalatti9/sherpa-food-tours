04f95e10f836e31ee6780d944c1d72ca27fd1775
GBalatti9 <gas.balatti@gmail.com>
2025-08-15 17:33:03 -0300

    home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,15 @@ import { getPageInfo } from "@/lib/wp";
 
 export default async function Home() {
   
-  const { title, content } = await getPageInfo("sherpa-food-tours")
-  console.log({title});
+  // const { title, content } = await getPageInfo("sherpa-food-tours")
+  // console.log({title});
   
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>{title}</h1>
-        <p dangerouslySetInnerHTML={{ __html: content }}></p>
+        <h1>Home</h1>
+        
       </main>
     </div>
   );
