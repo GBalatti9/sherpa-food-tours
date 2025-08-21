f579eca9ffec858a74b4741c550b0abcd778e7e8
GBalatti9 <gas.balatti@gmail.com>
2025-08-21 08:20:30 -0300

    content posts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { getPageInfo } from "@/lib/wp";
+import { getPageInfo, getPostInfo } from "@/lib/wp";
 // import Image from "next/image";
 
 // async function getStaticProps() {
@@ -14,15 +14,15 @@ import { getPageInfo } from "@/lib/wp";
 
 export default async function Home() {
   
-  // const { title, content } = await getPageInfo("sherpa-food-tours")
-  // console.log({title});
+  const { title, content } = await getPostInfo(43)
+  console.log({title});
   
 
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <h1>Home</h1>
-        
+        <h1>{title}</h1>
+        <div dangerouslySetInnerHTML={{ __html: content }}></div>
       </main>
     </div>
   );
--- src/lib/wp.ts
+++ src/lib/wp.ts
@@ -12,3 +12,18 @@ export const getPageInfo = async (slug: string) => {
 
     return { title, content };
 }
+
+export const getPostInfo = async (id: number) => {
+    const response = await fetch(`${apiUrl}/posts/${id}`)
+
+    if (!response.ok) throw new Error("No se obtuvieron datos");
+    const data = await response.json();
+    console.log({data});
+    
+
+    // const [data] = await response.json();
+
+    const { title: { rendered: title }, content: { rendered: content } } = data;
+
+    return { title, content };
+}
